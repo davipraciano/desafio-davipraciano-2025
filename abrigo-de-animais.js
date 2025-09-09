df9c0bd048c2ff599db4fc8955f63a6296654017
davipraciano <173275146+davipraciano@users.noreply.github.com>
2025-09-08 23:33:16 -0300

    Uma parte do Projeto

---
+++ abrigo-de-animais.js
@@ -0,0 +1,23 @@
+alert ("Olá, tudo certo? Me chamo Davi Almeida. E este é o projeto do Abrigo dos Animais que foi proposto na etapa do desafio técnico")
+
+
+let nome1 = "João"
+let brinquedo1 = ["Caixa", "Novelo", "Skate", "Rato"]
+let pessoaeanimal1 = ["Skate, Rato", "Caixa, Novelo", "Loco, Bola"]
+
+
+let nome2 = "Maria"
+let brinquedo2 = ["Bola", "Laser"]
+let pessoaeanimal2 = ["Bola, Laser", "Mimi"]
+
+
+let animais = ["Bola", "Bebe", "Fofo", "Loco", "Mimi", "Rex", "Zero"]
+
+let lista = ["Rato", "Bola", "Laser", "Caixa", "Novelo", "Skate"];
+
+
+console.log(animais)
+console.log(pessoaeanimal1[2], "-", nome1)
+console.log(pessoaeanimal2[1], "-", nome2)
+
+
