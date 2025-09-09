41862d0bf74331541159a4645ab7c83fe35647ee
davipraciano <173275146+davipraciano@users.noreply.github.com>
2025-09-09 11:52:19 -0300

    Projeto Abrigo-de-Animais

--- abrigo-de-animais.js
+++ abrigo-de-animais.js
@@ -16,8 +16,45 @@ let animais = ["Bola", "Bebe", "Fofo", "Loco", "Mimi", "Rex", "Zero"]
 let lista = ["Rato", "Bola", "Laser", "Caixa", "Novelo", "Skate"];
 
 
-console.log(animais)
+console.log("Animais:", "Bola", "-", "Bebe", "-", "Fofo","-", "Loco", "-", "Mimi", "-", "Rex", "-", "Zero")
+console.log("Brinquedos:", "Rato", "-", "Bola", "-","Laser", "-", "Caixa", "-", "Novelo", "-", "Skate")
 console.log(pessoaeanimal1[2], "-", nome1)
 console.log(pessoaeanimal2[1], "-", nome2)
+console.log("Ficaram 4 animais para adotar (tadinhos)")
 
+let animalinvalido = "Davi"
+if (animalinvalido == animais[0]) {
+    console.log("Animal válido")
+} else if (animalinvalido == animais[1]) {
+    console.log("Animal válido")
+} else if (animalinvalido == animais[2]){
+    console.log("Animal válido")
+} else if (animalinvalido == animais[3]) {
+    console.log("Animal válido")
+} else if (animalinvalido == animais[4]) {
+    console.log("Animal válido")
+} else if (animalinvalido == animais[5]) {
+    console.log("Animal válido")
+} else if (animalinvalido == animais[6]) {
+    console.log("Animal válido")
+} else {
+    console.log("Animal inválido")
+}
+    
+let brinquedoinvalido = "Casa"
+if (brinquedoinvalido == lista[0]) {
+    console.log("Brinquedo válido")
+} else if (brinquedoinvalido == lista[1]) {
+    console.log("Brinquedo válido")
+} else if (brinquedoinvalido == lista[2]){
+    console.log("Brinquedo válido")
+} else if (brinquedoinvalido == lista[3]) {
+    console.log("Brinquedo válido")
+} else if (brinquedoinvalido == lista[4]) {
+    console.log("Brinquedo válido")
+} else if (brinquedoinvalido == lista[5]) {
+    console.log("Brinquedo válido")
+} else {
+    console.log("Brinquedo inválido")
+}
 
